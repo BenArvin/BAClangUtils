1e03025d20af0931a9df1450bd36c058f4d34551
BenArvin <niedongsen@yeah.net>
2019-02-28 17:13:16 +0800

    feat: upspeed

--- BAClangUtils/ShellUtil.py
+++ BAClangUtils/ShellUtil.py
@@ -9,14 +9,5 @@ class ShellUtil(object):
     def runShell(cls, cmd):
         if cmd == None or isinstance(cmd, str) == False or len(cmd) < 1:
             return None
-        
-        cmdOutput = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
-
-        output = cmdOutput.stdout.read().decode(encoding='utf-8')
-        cmdOutput.stdout.close()
-
-        error = cmdOutput.stderr.read().decode(encoding='utf-8')
-        cmdOutput.stderr.close()
-
-        cmdOutput.wait()
-        return {'output': output, 'error': error}
+        output, error = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1).communicate()
+        return {'output': output.decode(encoding='utf-8'), 'error': error.decode(encoding='utf-8')}
