dcf62fd14c9849165961cec2051208db153e1bce
arvinnie <arvinnie@tencent.com>
2021-02-19 17:38:15 +0800

    feat: try fix tarvis build error

--- travis/travis_install.sh
+++ travis/travis_install.sh
@@ -3,3 +3,6 @@
 sudo apt update
 sudo apt-get install python3-pip
 sudo python3 -m pip install --upgrade pip setuptools wheel
+
+virtualenv -p /usr/bin/python3 venv/py3 --verbose --no-setuptools
+python3 -m pip install --upgrade 'setuptools; python_version >= "3.6"' 'setuptools<51.3.0; python_version < "3.6" and python_version >= "3.0"'
