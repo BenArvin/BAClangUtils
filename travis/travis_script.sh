85bee3c0e465a6a4b96d710cc3c7278215f78ece
arvinnie <arvinnie@tencent.com>
2021-02-19 18:50:53 +0800

    feat: try fix travis build error

--- travis/travis_script.sh
+++ travis/travis_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-virtualenv -p /usr/bin/python3 venv/py3 --verbose --no-setuptools
-python3 -m pip install --upgrade 'setuptools; python_version >= "3.6"' 'setuptools<51.3.0; python_version < "3.6" and python_version >= "3.0"'
-
 #remove last build result
 sudo rm -rf /build/*
 sudo rm -rf /dist/*
